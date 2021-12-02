8ccb598cc1ce236b4a929d088f2377de83164fd5
Tomek Karwowski <to.karwowski@gmail.com>
2021-12-02 16:59:48 +0100

    barebones library draft
    Signed-off-by: Tomek Karwowski <to.karwowski@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,87 @@
-mod utils;
-
+#![allow(unused_variables)]
+use js_sys::Reflect;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{
+    MessageEvent, RtcDataChannelEvent, RtcPeerConnection, RtcPeerConnectionIceEvent, RtcSdpType,
+    RtcSessionDescriptionInit,
+};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+macro_rules! console_warn {
+    ($($t:tt)*) => (warn(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rusty-games!");
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn warn(s: &str);
 }
+
+pub trait Message {}
+
+struct WebRtcConnection {}
+
+type MessageReceivedCallback = fn();
+
+pub struct Player {
+    pub id: String,
+    connection: WebRtcConnection,
+}
+
+pub struct Client {}
+
+impl Client {
+    fn new(message_received_callback: MessageReceivedCallback) -> Self {
+        Client {}
+    }
+}
+
+pub struct MiniServer {}
+
+impl MiniServer {
+    fn new(message_received_callback: MessageReceivedCallback) -> Self {
+        MiniServer {}
+    }
+}
+
+pub struct ClientSync<M: Message> {
+    messages_queue: Vec<M>,
+}
+
+impl<M: Message> ClientSync<M> {
+    /// Send a message to mini-server
+    pub fn send_message(message: M) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
+
+    /// Recieve all messages sent by mini-server
+    pub fn recieve_messages() -> Result<Vec<M>, Box<dyn std::error::Error>> {
+        Ok(vec![])
+    }
+}
+
+pub struct MiniServerSync<M: Message> {
+    messages_queue: Vec<M>,
+}
+
+impl<M: Message> MiniServerSync<M> {
+    /// Send a message to specified player
+    pub fn send_message(player: &Player, message: M) -> Result<(), Box<dyn std::error::Error>> {
+        Ok(())
+    }
+
+    /// Recieve all messages sent by players since last invocation
+    pub fn recieve_messages<T: Message>() -> Result<Vec<M>, Box<dyn std::error::Error>> {
+        Ok(vec![])
+    }
+}
+
+struct NetworkManager {}
+
+impl NetworkManager {}
