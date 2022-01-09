ed15b41378de7fc0abc5009dda97c3929a09f2b1
Tomek Karwowski <to.karwowski@gmail.com>
2022-01-09 15:27:37 +0100

    introduce seperate projects for client and server wasm binaries
    Signed-off-by: Tomek Karwowski <to.karwowski@gmail.com>

---
+++ library-client/src/lib.rs
@@ -0,0 +1,17 @@
+use log::info;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
+
+use rusty_games_library::network_manager::{ConnectionType, NetworkManager};
+use rusty_games_library::common::set_panic_hook;
+
+#[wasm_bindgen(start)]
+pub async fn main() -> Result<(), JsValue> {
+    set_panic_hook();
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
+
+    info!("wasm client started");
+    let client = NetworkManager::start("TODO-session-id".to_string(), ConnectionType::Local, false)?;
+
+    Ok(())
+}
--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,7 +1,7 @@
-mod common;
+pub mod common;
 mod mini_client;
 mod mini_server;
-mod network_manager;
+pub mod network_manager;
 
 use log::{debug, info};
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -13,28 +13,20 @@ use crate::mini_client::MiniClient;
 use crate::mini_server::MiniServer;
 use crate::network_manager::{ConnectionType, NetworkManager};
 
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_name = isServer)]
-    fn is_server() -> bool;
-    #[wasm_bindgen(js_namespace = getHash)]
-    fn get_hash() -> String;
-}
-
-#[wasm_bindgen(start)]
-pub async fn main() -> Result<(), JsValue> {
-    set_panic_hook();
-
-    wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
-
-    debug!("wasm main started");
-
-    let server = NetworkManager::start("TODO-session-id".to_string(), ConnectionType::Local, true)?;
-    let client = NetworkManager::start("TODO-session-id".to_string(), ConnectionType::Local, false)?;
-
-    // server
-    //     .borrow()
-    //     .send_message("hello honey, I love you")?;
-
-    Ok(())
-}
+// #[wasm_bindgen(start)]
+// pub async fn main() -> Result<(), JsValue> {
+//     set_panic_hook();
+//
+//     wasm_logger::init(wasm_logger::Config::new(log::Level::Debug));
+//
+//     debug!("wasm main started");
+//
+//     let server = NetworkManager::start("TODO-session-id".to_string(), ConnectionType::Local, true)?;
+//     let client = NetworkManager::start("TODO-session-id".to_string(), ConnectionType::Local, false)?;
+//
+//     // server
+//     //     .borrow()
+//     //     .send_message("hello honey, I love you")?;
+//
+//     Ok(())
+// }
