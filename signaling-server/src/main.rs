499820f7fa687de89059b2bafdccaa0b9e33a5c6
Tomek Karwowski <to.karwowski@gmail.com>
2023-06-21 12:28:47 +0200

    chore: enable deployments to shuttle.rs

--- signaling-server/src/main.rs
+++ signaling-server/src/main.rs
@@ -1,31 +1,18 @@
-use std::env;
-use std::net::SocketAddr;
-use std::str::FromStr;
-
 use log::LevelFilter;
 use simplelog::{ColorChoice, Config, TermLogger, TerminalMode};
 use wasm_peers_signaling_server::router::{self, ServerState};
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    TermLogger::init(
+#[shuttle_runtime::main]
+async fn signaling_server() -> shuttle_axum::ShuttleAxum {
+    let _ = TermLogger::init(
         LevelFilter::Debug,
         Config::default(),
         TerminalMode::Mixed,
         ColorChoice::Auto,
-    )?;
+    );
 
     let server_state = ServerState::default();
-    let app = router::create(server_state);
+    let router = router::create(server_state);
 
-    let address = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "0.0.0.0:9001".to_string());
-    let address = SocketAddr::from_str(&address)?;
-
-    axum::Server::bind(&address)
-        .serve(app.into_make_service())
-        .await?;
-
-    Ok(())
+    Ok(router.into())
 }
